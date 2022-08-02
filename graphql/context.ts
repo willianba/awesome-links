fdb916828a43f05072124f87489d0d7618445791
Willian Alves <wba.alves@gmail.com>
2022-08-02 00:22:11 -0300

    add auth0 integration | add basic auth

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,12 +1,25 @@
+import { Claims, getSession } from "@auth0/nextjs-auth0";
 import { PrismaClient } from "@prisma/client";
 import { prisma } from "../lib/prisma";
 
 export type Context = {
+  user?: Claims;
+  accessToken?: string;
   prisma: PrismaClient;
 };
 
-export async function createContext(): Promise<Context> {
+export async function createContext({ req, res }): Promise<Context> {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return { prisma };
+  }
+
+  const { user, accessToken } = session;
+
   return {
+    user,
+    accessToken,
     prisma,
   };
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
