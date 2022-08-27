015ba93d289bd86a0974b7b25c505412b0ef35e6
Willian Alves <wba.alves@gmail.com>
2022-08-26 23:42:14 -0300

    update project to deploy to vercel

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { relayStylePagination } from "@apollo/client/utilities";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
-  cache: new InMemoryCache(),
+  uri: "/api/graphql",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          links: relayStylePagination(),
+        },
+      },
+    },
+  }),
 });
 
 export default apolloClient;
